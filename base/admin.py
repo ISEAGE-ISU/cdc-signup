42571b3711740ba2d9e103148bcf0848921a10fd
Zach Heilman <organman91@gmail.com>
2014-09-12 00:20:32 -0500

    incomplete work on admin

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 import base.models as base_models
+from django.http import HttpResponse
+
+class ParticipantAdmin(admin.ModelAdmin):
+    actions = ['get_participant_emails']
+
+    def get_participant_emails(self, request, queryset):
+        emails = []
+        for participant in queryset:
+            user = participant.user
+            if not user.is_superuser:
+                emails.append(user.email)
+        return HttpResponse(content=emails)
+
+    get_participant_emails.short_description = "Get email list"
+
+
 
 # Register your models here.
-admin.site.register(base_models.Participant)
+admin.site.register(base_models.Participant, ParticipantAdmin)
 admin.site.register(base_models.Team)
 admin.site.register(base_models.GlobalSettings)
