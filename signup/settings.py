aded91c1c7fddb932e68c9fc8e15368c5fb796b4
Zach Heilman <organman91@gmail.com>
2015-06-20 13:59:33 -0500

    upgrading to django 1.8

--- signup/settings.py
+++ signup/settings.py
@@ -141,16 +141,15 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'base',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    #'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
 )
 
 LOGIN_URL = URL_ROOT + 'login/'
