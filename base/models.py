2efcd420a58408c677bf51c0eb7b7b471fd473ff
Keane O'Kelley <korockinout13@gmail.com>
2015-08-23 20:59:16 -0500

    Blank goes in models, not forms

--- base/models.py
+++ base/models.py
@@ -11,7 +11,7 @@ class GlobalSettings(models.Model):
     administrator_bind_pw = models.CharField(max_length=100)
     check_in_date = models.DateTimeField(null=True)
     enable_account_creation = models.BooleanField(default=True)
-    documentation_url = models.CharField(max_length=200, null=True)
+    documentation_url = models.CharField(max_length=200, blank=True, null=True)
 
 
 class Team(models.Model):
