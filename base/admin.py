272dacaf6e5dc7bafc7b10c58dfc71216563d9bc
Matt Gerst <mattgerst@gmail.com>
2015-12-12 01:09:40 -0600

    Add export to CSV option to participant admin

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 import base.models as base_models
 from django.http import HttpResponse
+import csv
+from cStringIO import StringIO
 
 
 class ParticipantAdmin(admin.ModelAdmin):
@@ -8,8 +10,22 @@ class ParticipantAdmin(admin.ModelAdmin):
     actions = [
         'get_participant_emails',
         'check_in',
-        'undo_check_in'
+        'undo_check_in',
+        'export_csv',
     ]
+
+    def export_csv(self, request, queryset):
+        si = StringIO()
+        cw = csv.writer(si)
+        cw.writerow(["Full Name", "Username", "Email", "Team Name", "Captain"])
+
+        for participant in queryset:
+            user = participant.user
+            if not user.is_superuser and participant.team:
+                cw.writerow([user.get_full_name(), user.username, user.email, participant.team, participant.captain])
+
+        return HttpResponse(content=si.getvalue(), content_type="text/plain")
+    export_csv.short_name = "Export CSV of Participants"
 
     def get_participant_emails(self, request, queryset):
         emails = []
