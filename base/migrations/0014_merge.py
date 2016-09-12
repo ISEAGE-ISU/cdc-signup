33881754a1835f3bae0779a3731826e71343669f
Matt Gerst <mattgerst@gmail.com>
2016-09-12 15:30:57 -0500

    Merge conflicting migrations

---
+++ base/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0013_auto_20160910_1454'),
+        ('base', '0011_participant_approved'),
+    ]
+
+    operations = [
+    ]
