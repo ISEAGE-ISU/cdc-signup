9222aca93f600a3f7c854ba31e8c03f4defed18e
Matt Gerst <mattgerst@gmail.com>
2017-02-20 18:29:16 -0600

    Fix migration conflicts

---
+++ base/migrations/0022_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0021_auto_20170207_1325'),
+        ('base', '0019_team_disbanded'),
+    ]
+
+    operations = [
+    ]
