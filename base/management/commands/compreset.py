4c7771b290b222ae215f2fa589ed5e7c813ca0c7
Logan J. Woolery <woolery@iseage.org>
2019-09-06 23:14:32 -0500

    Update compreset.py

--- base/management/commands/compreset.py
+++ base/management/commands/compreset.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        models.Team.objects.all().delete()
         User.objects.exclude(is_superuser=True).delete()
+        models.Team.objects.all().delete()
         models.ArchivedEmail.objects.all().delete()
         self.stdout.write("Competition reset performed")
